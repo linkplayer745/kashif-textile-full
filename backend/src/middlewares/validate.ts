86fe7d235daafe12387ba677c44bd92609055132
Talha Khan <linkplayer745@gmail.com>
2025-05-30 18:40:55 +0500

    deploying

--- backend/src/middlewares/validate.ts
+++ backend/src/middlewares/validate.ts
@@ -13,7 +13,7 @@ interface ValidationSchema {
 export const validate =
   (schema: ValidationSchema) =>
   (req: Request, res: Response, next: NextFunction) => {
-    const validSchema = pick(schema, ['params', 'query', 'body'] as const);
+    const validSchema = pick(schema, ['params', 'query', 'body']);
     const object = pick(req, Object.keys(validSchema) as (keyof Request)[]);
     const { value, error } = Joi.compile(validSchema)
       .prefs({ errors: { label: 'key' }, abortEarly: false })
